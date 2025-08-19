1c43312f0d83371ca48c3a589da67f6afb3f83fb
milan-varnamo <milan.mlinar.sv@gmail.com>
2025-08-19 19:11:07 +0200

    Enable CORS for localhost:3000 (HTTP & HTTPS)

--- API/Program.cs
+++ API/Program.cs
@@ -12,9 +12,13 @@ builder.Services.AddDbContext<AppDbContext>(opt =>
     }
 );
 
+builder.Services.AddCors();
+
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
+
+app.UseCors(x => x.AllowAnyHeader().AllowAnyMethod().WithOrigins("http://localhost:3000", "https://localhost:3000"));
 
 app.MapControllers();
 
