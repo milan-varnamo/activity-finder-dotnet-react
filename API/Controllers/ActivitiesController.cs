923091fb534c7c8c0ef3fbf3fe245a120809cca0
milan-varnamo <milan.mlinar.sv@gmail.com>
2025-08-20 17:56:42 +0200

    Add a mediator to GetActivityDetail endpoint

--- API/Controllers/ActivitiesController.cs
+++ API/Controllers/ActivitiesController.cs
@@ -2,11 +2,10 @@ using Application.Activities.Queries;
 using Domain;
 using MediatR;
 using Microsoft.AspNetCore.Mvc;
-using Persistence;
 
 namespace API.Controllers
 {
-    public class ActivitiesController(AppDbContext context, IMediator mediator) : BaseApiController
+    public class ActivitiesController(IMediator mediator) : BaseApiController
     {
         [HttpGet]
         public async Task<ActionResult<List<Activity>>> GetActivities()
@@ -16,11 +15,7 @@ namespace API.Controllers
         [HttpGet("{id}")]
         public async Task<ActionResult<Activity>> GetActivityDetail(string id)
         {
-            var activity = await context.Activities.FindAsync(id);
-
-            if (activity == null) return NotFound();
-
-            return activity;
+            return await mediator.Send(new GetActivityDetails.Query { Id = id });
         }
     }
 }
