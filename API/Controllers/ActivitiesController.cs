4f36975f984ebbf64f522179328560e173e28c95
milan-varnamo <milan.mlinar.sv@gmail.com>
2025-08-20 18:58:00 +0200

    Add CreateActivity endpoint

--- API/Controllers/ActivitiesController.cs
+++ API/Controllers/ActivitiesController.cs
@@ -1,3 +1,4 @@
+using Application.Activities.Commands;
 using Application.Activities.Queries;
 using Domain;
 using MediatR;
@@ -16,6 +17,11 @@ namespace API.Controllers
         public async Task<ActionResult<Activity>> GetActivityDetail(string id)
         {
             return await Mediator.Send(new GetActivityDetails.Query { Id = id });
+        }
+        [HttpPost]
+        public async Task<ActionResult<string>> CreateActivity(Activity activity)
+        {
+            return await Mediator.Send(new CreateActivity.Command{Activity = activity});
         }
     }
 }
