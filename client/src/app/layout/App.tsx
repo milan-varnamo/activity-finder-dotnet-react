9a3d80d8256b9b5fada07de2e6169b8a4f6f076c
milan-varnamo <milan.mlinar.sv@gmail.com>
2025-08-20 22:31:59 +0200

    Add NavBar component

--- client/src/app/layout/App.tsx
+++ client/src/app/layout/App.tsx
@@ -1,6 +1,13 @@
-import { List, ListItem, ListItemText, Typography } from '@mui/material';
+import {
+	Container,
+	CssBaseline,
+	List,
+	ListItem,
+	ListItemText,
+} from '@mui/material';
 import axios from 'axios';
 import { useEffect, useState } from 'react';
+import NavBar from './NavBar';
 
 function App() {
 	const [activities, setActivities] = useState<Activity[]>([]);
@@ -13,14 +20,17 @@ function App() {
 
 	return (
 		<>
-			<Typography variant='h3'>ActivityFinder</Typography>;
-			<List>
-				{activities.map((activity) => (
-					<ListItem key={activity.id}>
-						<ListItemText>{activity.title}</ListItemText>
-					</ListItem>
-				))}
-			</List>
+			<CssBaseline />
+			<NavBar />
+			<Container maxWidth='xl' sx={{ mt: 3 }}>
+				<List>
+					{activities.map((activity) => (
+						<ListItem key={activity.id}>
+							<ListItemText>{activity.title}</ListItemText>
+						</ListItem>
+					))}
+				</List>
+			</Container>
 		</>
 	);
 }
