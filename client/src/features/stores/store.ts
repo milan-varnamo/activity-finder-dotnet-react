60720024d8a471887fcbc83f12b64e8aebcdc087
milan-varnamo <milan.mlinar.sv@gmail.com>
2025-08-22 16:58:28 +0200

    Add loading bar to NavBar

---
+++ client/src/features/stores/store.ts
@@ -0,0 +1,12 @@
+import { createContext } from 'react';
+import { UIStore } from './UIStore';
+
+interface Store {
+	uiStore: UIStore;
+}
+
+export const store: Store = {
+	uiStore: new UIStore(),
+};
+
+export const StoreContext = createContext(store);
